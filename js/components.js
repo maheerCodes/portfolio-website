01129e30aa670517bcdfa706a5200aa9d0aaaaa3
Sheikh Mohammad Ali Maheer <maheer.codes@outlook.com>
2026-06-07 08:25:43 +0600

    navLower_Foundation

--- js/components.js
+++ js/components.js
@@ -15,25 +15,58 @@ window.addEventListener("scroll", () => {
 
 
 
-
+let isInactive = false;                                          // NEW
+let mouseTriggered = false;                                      // NEW
 
 function showNavLower() {
     navLower.style.bottom = "0vh";
+    navLower.style.transition = "bottom 0.9s ease-in-out, opacity 1s ease-in-out";                             // NEW
+    navLower.style.opacity = "1";                                // NEW
     if (hideTimeout) clearTimeout(hideTimeout);
     hideTimeout = null; // Clear the timeout reference
 }
 
 function hideNavLowerWithDelay(x = 500) {
-    if (hideTimeout) clearTimeout(hideTimeout);
+    if (hideTimeout) 
+        {
+        clearTimeout(hideTimeout);
+    }
     hideTimeout = setTimeout(() => {
-        navLower.style.bottom = "-8vh";
+        // navLower.style.bottom = "-8vh";                                // NEW
         navLower.style.opacity = "0.6";
-        navLower.style.transition = "bottom 0.3s ease-in-out";
+        navLower.style.transition = "opacity 1s ease-in-out";
+        isInactive = true;                                          // NEW
+        // navLower.style.transition = "bottom 5s ease-in-out";
     }, x);
 }
 showNavLower(); // Show navLower on page load
 hideNavLowerWithDelay(5000); // Hide navLower after 5 seconds
+
+window.addEventListener("mousemove", () => {
+    if (!isInactive) return;
+
+    if (!mouseTriggered) {
+        navLower.style.transition = "bottom 0.9s ease-in-out";
+        navLower.style.bottom = "-8vh";
+        mouseTriggered = true;
+    }
+});
+
 navLowerTransparent.addEventListener("mouseenter", showNavLower);
-navLowerTransparent.addEventListener("mouseleave", hideNavLowerWithDelay);
+
+navLowerTransparent.addEventListener("mouseleave", () => {
+    if (mouseTriggered) {
+        navLower.style.bottom = "-8vh";
+    }
+});
+
 navLower.addEventListener("mouseenter", showNavLower);
-navLower.addEventListener("mouseleave", hideNavLowerWithDelay);
+
+navLower.addEventListener("mouseleave", () => {
+    if (mouseTriggered) {
+        navLower.style.transition = "bottom 0.3s ease-in-out";
+        navLower.style.bottom = "-8vh";
+    }
+});
+// navLower.addEventListener("mouseenter", showNavLower);
+// navLower.addEventListener("mouseleave", hideNavLowerWithDelay);
